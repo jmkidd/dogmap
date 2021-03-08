fcceffa1736faf70ffa82635c791c027066d1e8e
Jeffrey Kidd <jmkidd@umich.edu>
2021-03-08 10:13:16 -0500

    gather stats

--- run-stats.py
+++ run-stats.py
@@ -10,6 +10,8 @@ import argparse
 import time
 import socket
 import shutil
+import gzip
+import numpy as np
 
 ###############################################################################
 # Helper function to run commands, handle return values and print to log file
@@ -113,7 +115,119 @@ def run_genotype_known_sites(myData):
     else:
         print('%s exists, genotype known sites' % myData['knownSitesVCF'],flush=True)    
 ###############################################################################
+def calc_effective_depth(myData):
+    print('\nrunning calc effective depth ...', flush=True)
+    myData['depthSummary'] = myData['cramFileName'] + '.knownsites.depth.txt'
+    
+    if os.path.isfile(myData['depthSummary']) is True:
+        print('%s exists' % myData['depthSummary'],flush=True)    
+        return
+    autoDp = []
+    xDp = []        
+    inFile = gzip.open(myData['knownSitesVCF'],'rt')
+    for line in inFile:
+        if line[0] == '#':
+            continue
+        line = line.rstrip()
+        line = line.split()
+        
+        infoField = line[7]
+        infoField = infoField.split(';')
+        dp = -1
+        for i in infoField:
+            if i[0:3] == 'DP=':
+                dp = int(i.split('=')[1])
+                break
 
+        if dp == -1:
+            dp = 0
+        if line[0] == 'chrX':
+            xDp.append(dp)
+        else:
+            autoDp.append(dp)
+    inFile.close()
+    
+    outFile = open(myData['depthSummary'],'w')
+        
+    outFile.write('#chrom\ttotalSites\tMean\tStd\tMedian\n')
+    outFile.write('Autos\t%i\t%.2f\t%.2f\t%.1f\n' % (len(autoDp),np.mean(autoDp),np.std(autoDp),np.median(autoDp) ))
+    outFile.write('ChrX\t%i\t%.2f\t%.2f\t%.1f\n' % (len(xDp),np.mean(xDp),np.std(xDp),np.median(xDp) ))
+    outFile.close()
+###############################################################################
+def summarize_stats(myData):
+    myData['statsSummary'] = myData['cramFileName'] + '.stats.txt'
+    
+    outFile = open(myData['statsSummary'],'w')
+    
+    sn = myData['cramFileName'].split('/')[-1].split('.')[0]
+    
+    outFile.write('SampleName\t%s\n' % (sn))
+    
+    cramFileSize = os.path.getsize(myData['cramFileName']) / (1024*1024*1024)
+    outFile.write('CramSize\t%.2f Gb\n' % cramFileSize)
+
+    cramFileSize = os.path.getsize(myData['gvcf']) / (1024*1024*1024)
+    outFile.write('GVCFSize\t%.2f Gb\n' % cramFileSize)
+    
+    # now get coverage stats
+    
+    inFile = open(myData['depthSummary'],'r')
+    lines=inFile.readlines()
+    inFile.close()
+    
+    autoLine = lines[1].rstrip().split()
+    xLine = lines[2].rstrip().split()
+    
+    outFile.write('effectiveAutoMean\t%s\neffectiveAutoMedian\t%s\n' % (autoLine[2],autoLine[4]) )
+    outFile.write('effectiveXMean\t%s\neffectiveXMedian\t%s\n' % (xLine[2],xLine[4]) )
+    
+    xvsAutoMean =  float(autoLine[2])/ float(xLine[2])
+    xvsAutoMedian = float(autoLine[4])/ float(xLine[4])
+    
+    outFile.write('Mean(X/Auto)\t%.2f\n' % xvsAutoMean)
+    outFile.write('Median(X/Auto)\t%.2f\n' % xvsAutoMedian)
+    
+    # dup mark stats
+    # now get mark dup metrics
+    dupMetFileName = myData['cramFileName'].replace('.cram','.sort.md.metricts.txt')
+    inFile = open(dupMetFileName,'r')
+    lines = inFile.readlines()
+    inFile.close()
+
+    metLine = lines[7].rstrip().split()
+    dupF = metLine[8]
+   
+    outFile.write('fractionDup\t%s\n' % dupF) 
+    
+    # now get read alignment stats
+    
+    inFile = open(myData['alignMetricsFileName'],'r')
+    lines = inFile.readlines()
+    pairLine = lines[9].rstrip().split()
+    inFile.close()
+    
+    outFile.write('totalPairedReads\t%s\n' % pairLine[1])
+    outFile.write('fractionAligned\t%s\n' % pairLine[6])
+    outFile.write('mismatchRate\t%s\n' % pairLine[12])
+    outFile.write('indelRate\t%s\n' % pairLine[14])
+    outFile.write('meanReadLen\t%s\n' % pairLine[15])
+    outFile.write('fractionImproperPairs\t%s\n' % pairLine[19])
+    outFile.write('fractionChimera\t%s\n' % pairLine[22])
+
+    # now get insert len stats
+    inFile = open(myData['sizeMetricsFileName'],'r')
+    lines = inFile.readlines()
+    inFile.close()
+    
+    statLine = lines[7].rstrip().split()
+    
+    outFile.write('meanInsertLen\t%s\n' % statLine[5])
+    outFile.write('stdInsertLen\t%s\n' % statLine[6])
+    outFile.write('medianInsertLen\t%s\n' % statLine[0])
+    outFile.write('madInsertLen\t%s\n' % statLine[2])
+    outFile.close()    
+    print('Summary written to',myData['statsSummary'])
+###############################################################################
 
 # SETUP
 
@@ -137,11 +251,12 @@ myData['sites'] = args.sites
 
 # make sure programs are availble
 check_prog_paths(myData)
-
 run_flagstat(myData)
 run_insertmetrics(myData)
 run_alignmentmetrics(myData)
 run_index_cram(myData)
 run_index_gvcf(myData)
 run_genotype_known_sites(myData)
+calc_effective_depth(myData)
+summarize_stats(myData)
 
